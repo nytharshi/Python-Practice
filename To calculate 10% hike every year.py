b3f1253f6d134d0f5806e3c53346f8207dbd7c60
nytharshi <harshit.taneja1212@gmail.com>
2026-09-22 01:30:03 +0530

    Update Python practice

--- To calculate 10% hike every year.py
+++ To calculate 10% hike every year.py
@@ -1,4 +1,4 @@
-salary = 1000
+salary=int(input("Enter your salary"))
 for i in range(1, 4):
     salary = salary * 1.10
     print(f"Year {i} salary: {salary:.2f}")
---
+++ Write a program to check whether a number is divisible by 5 and 11.py
@@ -0,0 +1,7 @@
+x=int(input("Enter a number"))
+if x//5==0:
+    print("It is divisible by 5")
+elif x//11:
+    print("It is divisible by 11")
+else:
+    print("It is not divisible by 5 or 11")
